abef5f3b714ca2dacbd0cd3a7208ab83d0b71805
andrewfaulkner <andrewfau1kn3r@gmail.com>
2026-02-11 02:08:16 +0800

    make less waves and tankier enemies for debugging

--- Managers/WaveManager.cs
+++ Managers/WaveManager.cs
@@ -53,7 +53,7 @@ public class WaveManager
         {
             new(
                 EnemyCount: 5,
-                EnemyHealth: 30,
+                EnemyHealth: 300,
                 EnemySpeed: 90,
                 EnemyBounty: 5,
                 SpawnInterval: 1.0f,
@@ -61,7 +61,7 @@ public class WaveManager
             ),
             new(
                 EnemyCount: 8,
-                EnemyHealth: 40,
+                EnemyHealth: 400,
                 EnemySpeed: 95,
                 EnemyBounty: 5,
                 SpawnInterval: 0.9f,
@@ -69,7 +69,7 @@ public class WaveManager
             ),
             new(
                 EnemyCount: 10,
-                EnemyHealth: 60,
+                EnemyHealth: 600,
                 EnemySpeed: 100,
                 EnemyBounty: 8,
                 SpawnInterval: 0.8f,
@@ -77,7 +77,7 @@ public class WaveManager
             ),
             new(
                 EnemyCount: 12,
-                EnemyHealth: 80,
+                EnemyHealth: 800,
                 EnemySpeed: 110,
                 EnemyBounty: 8,
                 SpawnInterval: 0.8f,
@@ -85,51 +85,11 @@ public class WaveManager
             ),
             new(
                 EnemyCount: 15,
-                EnemyHealth: 100,
+                EnemyHealth: 1000,
                 EnemySpeed: 120,
                 EnemyBounty: 10,
                 SpawnInterval: 0.7f,
                 AttackDamage: 12
-            ),
-            new(
-                EnemyCount: 18,
-                EnemyHealth: 130,
-                EnemySpeed: 130,
-                EnemyBounty: 10,
-                SpawnInterval: 0.7f,
-                AttackDamage: 12
-            ),
-            new(
-                EnemyCount: 20,
-                EnemyHealth: 170,
-                EnemySpeed: 135,
-                EnemyBounty: 12,
-                SpawnInterval: 0.6f,
-                AttackDamage: 18
-            ),
-            new(
-                EnemyCount: 22,
-                EnemyHealth: 220,
-                EnemySpeed: 140,
-                EnemyBounty: 15,
-                SpawnInterval: 0.6f,
-                AttackDamage: 18
-            ),
-            new(
-                EnemyCount: 25,
-                EnemyHealth: 300,
-                EnemySpeed: 145,
-                EnemyBounty: 18,
-                SpawnInterval: 0.5f,
-                AttackDamage: 25
-            ),
-            new(
-                EnemyCount: 30,
-                EnemyHealth: 400,
-                EnemySpeed: 145,
-                EnemyBounty: 25,
-                SpawnInterval: 0.4f,
-                AttackDamage: 25
             ),
         };
     }
